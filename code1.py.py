0f662a975e0855bb13b8607189ff8a7c37ab9b99
mahsa <mahsa.fazeli6633@gmail.com>
2026-08-18 17:12:51 +0330

    Add files via upload

---
+++ code1.py.py
@@ -0,0 +1,5 @@
+fruit_price= {"apple" :1500 ,"banana":1000 , "orange":1200}
+fruit_price["banana"]=1100
+del fruit_price["apple"]
+
+print(fruit_price)
---
+++ code3.py.py
@@ -0,0 +1,9 @@
+import math
+
+coordinates = (4, 3)
+
+x, y = coordinates
+
+distance = math.sqrt((x - 0)**2 + (y - 0)**2)
+
+print(distance)
